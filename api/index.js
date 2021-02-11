fdcaa3c32e8ad0d3377d5af343e14f34ce4d75b4
Mark Carey <markallencarey@gmail.com>
2021-02-11 15:55:20 -0700

    postman testing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const moviesCtrl = require('./moviesController')
 
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
@@ -9,7 +10,10 @@ app.get('/test', function (req, res) {
   res.send('Test successful')
 })
 
-app.get('/allMovies', moviesCtrl.getAllMovies)
+app.get('/testMovie', moviesCtrl.getTestMovie)
+app.get('/movies', moviesCtrl.getTenMovies)
+
+app.get('/pokemon', moviesCtrl.getPokemon)
 
 export default {
   path: '/api',
--- api/moviesController.js
+++ api/moviesController.js
@@ -1,9 +1,31 @@
+require('dotenv').config()
 const axios = require('axios')
 
+const { API_KEY } = process.env
+// const { title } = 
+const title = 'star'
+
 module.exports = {
-  getAllMovies: (req, res) => {
-    axios.get().then(res => {
-      res.status(200).send(res.data)
+  getTestMovie: (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/2?api_key=${API_KEY}&language=en-US`).then(axRes => {
+      res.status(200).send(axRes.data)
+    }).catch(err => console.log(err))
+  },
+
+  getTenMovies: (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${title}&include_adult=false&region=US`).then(axRes => {
+      let movies = axRes.data.results
+      console.log(movies)
+      // if (axRes.data.results.length > 10) {
+
+      // }
+      res.status(200).send(movies)
+    }).catch(err => console.log(err))
+  },
+
+  getPokemon: (req, res) => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1').then(axRes => {
+      res.status(200).send(axRes.data)
     }).catch(err => console.log(err))
   }
 }
